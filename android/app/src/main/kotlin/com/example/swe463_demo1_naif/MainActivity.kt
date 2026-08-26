8992e3bb872f807e968066575f40255c5161eb91
kjmoly <stvsar1237@gmail.com>
2026-08-26 21:40:31 +0300

    Complete SWE 463 Demo 1

---
+++ android/app/src/main/kotlin/com/example/swe463_demo1_naif/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.swe463_demo1_naif
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,205 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       home: Text('Naif Alenizi'),
+//     ),
+//   );
+// }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Text(
+//             'Welcome to SWE 463',
+//             style: TextStyle(fontSize: 24),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('SWE 463 - Demo 1'),
+//           backgroundColor: Colors.indigo,
+//           foregroundColor: Colors.white,
+//         ),
+//         drawer: const Drawer(
+//           child: SafeArea(
+//             child: Column(
+//               children: [
+//                 DrawerHeader(
+//                   child: Center(
+//                     child: Text(
+//                       'Demo Menu',
+//                       style: TextStyle(fontSize: 24),
+//                     ),
+//                   ),
+//                 ),
+//                 ListTile(
+//                   leading: Icon(Icons.home),
+//                   title: Text('Home'),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//         body: const Center(
+//           child: Text(
+//             'Open the drawer from the menu icon',
+//             style: TextStyle(fontSize: 20),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('SWE 463 - Demo 1'),
+//         ),
+//         drawer: const Drawer(
+//           child: SafeArea(
+//             child: ListTile(
+//               leading: Icon(Icons.home),
+//               title: Text('Home'),
+//             ),
+//           ),
+//         ),
+//         body: const Center(
+//           child: Text(
+//             'Hot reload is working!',
+//             style: TextStyle(fontSize: 24),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'SWE 463 Demo 1',
+//       home: const HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Home Page'),
+//       ),
+//       body: const Center(
+//         child: Text(
+//           'This is the Home Page',
+//           style: TextStyle(fontSize: 24),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class LoginPage extends StatelessWidget {
+//   const LoginPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Login Page'),
+//       ),
+//       body: const Center(
+//         child: Text(
+//           'This is the Login Page',
+//           style: TextStyle(fontSize: 24),
+//         ),
+//       ),
+//     );
+//   }
+// }
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage()));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to Login'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LoginPage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to home'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
